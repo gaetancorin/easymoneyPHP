2ef38484efef0975685432530917f646ecc6fdad
Adeline <adeline.mdg@gmail.com>
2022-04-27 16:36:08 +0200

    petit modif

--- index.php
+++ index.php
@@ -25,39 +25,33 @@
 <ul class="tilesWrap">
 	<li>
 		<h2><?php echo $date_game1 ?></h2>
-		<h3>Title 1</h3>
+		<h3>Les matchs arrivent</h3>
 		<p>
-			Lorem Ipsum is simply dummy text of the printing and typesetting   
-			industry. Lorem Ipsum has been the industry's standard dummy text ever 
-			since the 1500s.
+            ça arrive !
 		</p>
 		<button>Parier ?</button>
 	</li>
 	<li>
 		<h2>02</h2>
-		<h3>Title 2</h3>
+		<h3>Les matchs arrivent</h3>
 		<p>
-			When an unknown printer took a galley of type and scrambled it to make 
-			a type specimen book. It has survived not only five centuries.
+			on arrive très vite !
 		</p>
 		<button>Parier ?</button>
 	</li>
 	<li>
 		<h2>03</h2>
-		<h3>Title 3</h3>
+		<h3>Les matchs arrivent</h3>
 		<p>
-			But also the leap into electronic typesetting, remaining essentially 
-			unchanged. It was popularised in the 1960s.
+			on arrive très vite !
 		</p>
 		<button>Parier ?</button>
 	</li>
 	<li>
 		<h2>04</h2>
-		<h3>Title 4</h3>
+		<h3>Les matchs arrivent</h3>
 		<p>
-			With the release of Letraset sheets containing Lorem Ipsum passages,  
-			and more recently with desktop publishing software like Aldus PageMaker 
-			including versions of Lorem Ipsum.
+			on arrive très vite !
 		</p>
 		<button>Parier ?</button>
 	</li>
