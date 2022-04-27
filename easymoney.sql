248d4fb481853d5fd457631383895a57d98b8334
Kheandee <94051174+Kheandee@users.noreply.github.com>
2022-04-27 15:54:37 +0200

    Add databases

--- easymoney.sql
+++ easymoney.sql
@@ -2,10 +2,10 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Hôte : localhost:8889
--- Généré le : mer. 27 avr. 2022 à 09:33
--- Version du serveur : 5.7.34
--- Version de PHP : 7.4.21
+-- Hôte : 127.0.0.1
+-- Généré le : mer. 27 avr. 2022 à 15:52
+-- Version du serveur : 10.4.21-MariaDB
+-- Version de PHP : 8.0.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,6 +33,32 @@ CREATE TABLE `equipe` (
   `detail_equipe` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Déchargement des données de la table `equipe`
+--
+
+INSERT INTO `equipe` (`id_equipe`, `nom_equipe`, `detail_equipe`) VALUES
+(1, 'Meets', 'New York'),
+(2, 'Giants', 'San Francisco'),
+(3, 'Dodgers', 'Los Angeles'),
+(4, 'Blues Jays', 'Toronto'),
+(5, 'Angels', 'Los Angeles'),
+(6, 'Brewers', 'Milwaukee'),
+(7, 'Yankees', 'New York'),
+(8, 'Padres', 'San Diego'),
+(9, 'Mariners', 'Seattle'),
+(10, 'Rockies', 'Colorado'),
+(11, 'Twins', 'Minesota'),
+(12, 'Athletics', 'Oakland'),
+(13, 'Cardinals', 'St Louis'),
+(14, 'Rays', 'Tampa Bay'),
+(15, 'Braves', 'Atlanta'),
+(16, 'Astros', 'Houston'),
+(17, 'Marlins', 'Miami'),
+(18, 'Phillies', 'Philadelphie'),
+(19, 'Pirates', 'Pittsburgh'),
+(20, 'Diamondbacks', 'Arizona');
+
 -- --------------------------------------------------------
 
 --
@@ -45,6 +71,32 @@ CREATE TABLE `game` (
   `date_game` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Déchargement des données de la table `game`
+--
+
+INSERT INTO `game` (`id_game`, `nom_game`, `date_game`) VALUES
+(1, 'Meets vs Mariners', '2022-04-01'),
+(2, 'Giants vs Astros', '2022-04-03'),
+(3, 'Dodgers vs Cardinals', '2022-04-05'),
+(4, 'Blues Jays vs Brewers', '2022-04-06'),
+(5, 'Angels vs Rockies', '2022-04-08'),
+(6, 'Brewers vs Giants', '2022-04-09'),
+(7, 'Yankees vs Diamondbacks', '2022-04-10'),
+(8, 'Padres vs Yankees', '2022-04-11'),
+(9, 'Mariners vs Pirates', '2022-04-12'),
+(10, 'Rockies vs Angels', '2022-04-14'),
+(11, 'Twins vs Rays', '2022-04-15'),
+(12, 'Athletics vs Padres', '2022-04-18'),
+(13, 'Cardinals vs Twins', '2022-04-19'),
+(14, 'Rays vs Marlins', '2022-04-20'),
+(15, 'Brewers vs Phillies', '2022-04-24'),
+(16, 'Astros vs Dodgers', '2022-04-25'),
+(17, 'Marlins vs Blues Jays', '2022-04-27'),
+(18, 'Phillies vs Meets', '2022-04-28'),
+(19, 'Pirates vs Meets', '2022-04-30'),
+(20, 'Diamondbacks vs Athletics', '0000-00-00');
+
 -- --------------------------------------------------------
 
 --
@@ -56,6 +108,15 @@ CREATE TABLE `participer` (
   `id_game` int(11) NOT NULL,
   `point_equipe` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `participer`
+--
+
+INSERT INTO `participer` (`id_equipe`, `id_game`, `point_equipe`) VALUES
+(4, 17, 2),
+(5, 5, 4),
+(11, 13, 3);
 
 -- --------------------------------------------------------
 
@@ -71,6 +132,18 @@ CREATE TABLE `utilisateur` (
   `mot_de_passe` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`id_utilisateur`, `pseudo`, `prenom`, `nom`, `mot_de_passe`, `email`) VALUES
+(1, 'Ace', 'David', 'Esteban', 'desteban', 'aceteban31@gmail.com'),
+(2, 'TigreTamoul', 'Gokool', 'Lavasan', 'glavasan', 'sangokool@gmail.com'),
+(3, 'MiteMega', 'Jordan', 'Morin', 'jmorin', 'mitemega@gmail.com'),
+(4, 'Kheandee', 'Valentin', 'Dietrich', 'vdietrich', 'kheandee@gmail.com'),
+(5, 'KimK', 'Adeline', 'Mendouga', 'amendouga', 'realkim@gmail.com'),
+(6, 'JojoFront', 'Johan', 'Valero', 'jvalero', 'jojofront@gmail.com');
 
 --
 -- Index pour les tables déchargées
@@ -109,19 +182,19 @@ ALTER TABLE `utilisateur`
 -- AUTO_INCREMENT pour la table `equipe`
 --
 ALTER TABLE `equipe`
-  MODIFY `id_equipe` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_equipe` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT pour la table `game`
 --
 ALTER TABLE `game`
-  MODIFY `id_game` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_game` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
-  MODIFY `id_utilisateur` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_utilisateur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Contraintes pour les tables déchargées
