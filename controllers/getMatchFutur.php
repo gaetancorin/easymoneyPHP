510be31cf871f69db353a40bcab7f763b1f39eb2
gaetan <gaetancorin@hotmail.fr>
2022-04-29 12:14:34 +0200

    changement getMatchFutur pour avoir l equipe et la mise sur laquel on a parier

--- controllers/getMatchFutur.php
+++ controllers/getMatchFutur.php
@@ -1,14 +1,26 @@
 <?php 
 include("./models/game.php");
 include("./config/bdd.php");
-$game = new game();
-$games_futurs =$game->lire_games_futurs();
+include('./models/user.php');
 
-// lire_games_futurs() fournis un tableau ou chaque match représente 2 enregistrements(un pour chaque equipe).
+// Je vérifie si l utilisateur est connecter
+if (isset($_SESSION['pseudo'])){
+    $pseudo = $_SESSION['pseudo'];
+    $connexion=True;
+}
+else{
+    $connexion=False;
+}
+
+// Je lis tous les matchs finis
+$game = new game();
+$games_finis =$game->lire_games_futurs();
+
+// lire_games_finis() fournis un tableau ou chaque match représente 2 enregistrements(un pour chaque equipe).
 // On utilise un compteur qui s'incrémente, modulo %1 pour séparer les enregistrement 2 par 2(pour séparer chaque match)
 $compteur = 0;
 $matchs = [];
-while($donnees = $games_futurs->fetch()){
+while($donnees = $games_finis->fetch()){
 
     if ($compteur == 0){
         $nom_game = $donnees["nom_game"];
@@ -22,15 +34,62 @@ while($donnees = $games_futurs->fetch()){
         $nom_equipe2 = $donnees["nom_equipe"];
         $detail_equipe2 = $donnees["detail_equipe"];
 
+        // je me sert du nom de la game et du nom des equipes
+        // pour récupérer le pourcentage total de paris par equipe  
+        $game->set_nom_game($nom_game);
+        $pourcentage_mise = $game->calcul_pourcentage_miser_total($nom_equipe1,$nom_equipe2);
+        $pourcentage_equipe1 = $pourcentage_mise[0];
+        $pourcentage_equipe2 = $pourcentage_mise[1];
+
+
+        if ($connexion){
+            // je reinitialise la variable $equipeparier_mise 
+            $equipeparier_mise = 0;
+            // je recupère la mise et l equipe sur lequel a miser l'utilisateur 
+            // uniquement sur ce match
+            $myUser = new User();
+            $myUser->set_pseudo($pseudo); 
+            $equipeparier_mise = $myUser->get_infos_mise($nom_game);
+            if (isset($equipeparier_mise['mise'])){
+                $nom_equipe_parier = $equipeparier_mise['nom_equipe_parier'];
+                $mise = $equipeparier_mise['mise'];
+            }
+            
+        
+ 
+        }
+        
+
+
         // Une fois toutes les informations dans des variables, je le met dans un tableau associatif
-        $match = [
-            'nom_game' => $nom_game, 
-            'date_game' => $date_game, 
-            'nom_equipe1' => $nom_equipe1,
-            'detail_equipe1' => $detail_equipe1,
-            'nom_equipe2' => $nom_equipe2,
-            'detail_equipe2' => $detail_equipe2
-        ];
+        if (isset($equipeparier_mise['mise'])){
+            $match = [
+                'nom_game' => $nom_game, 
+                'date_game' => $date_game, 
+                'nom_equipe1' => $nom_equipe1,
+                'detail_equipe1' => $detail_equipe1,
+                'pourcentage_equipe1' => $pourcentage_equipe1,
+                'nom_equipe2' => $nom_equipe2,
+                'detail_equipe2' => $detail_equipe2,           
+                'pourcentage_equipe2' => $pourcentage_equipe2,
+                'nom_equipe_parier' => $nom_equipe_parier,
+                'mise' =>  $mise,
+            ];
+        }
+        else{
+            $match = [
+                'nom_game' => $nom_game, 
+                'date_game' => $date_game, 
+                'nom_equipe1' => $nom_equipe1,
+                'detail_equipe1' => $detail_equipe1,
+                'pourcentage_equipe1' => $pourcentage_equipe1,
+                'nom_equipe2' => $nom_equipe2,
+                'detail_equipe2' => $detail_equipe2,           
+                'pourcentage_equipe2' => $pourcentage_equipe2,
+
+                ];
+        }
+
         // Puis je met ce tableau dans un autre tableau contenant tous les matchs futur
         array_push($matchs, $match);
 
